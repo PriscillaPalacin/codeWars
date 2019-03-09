ed404568acb4d88a8a12cc82c7daab0ba871cecf
PriscillaPalacin <priscipal7993@icloud.com>
2019-03-08 23:00:07 -0500

    jennys secrect message

---
+++ jennys message /jennymsg.js
@@ -0,0 +1,16 @@
+function greet(name){
+  if(name === "Johnny")
+    return "Hello, my love!";
+    return "Hello, " + name + "!";
+}
+
+Test.describe("Jenny's greeting function", function(){
+  Test.it("should greet some people normally",function(){
+    Test.assertEquals(greet("Jim"), "Hello, Jim!");
+    Test.assertEquals(greet("Jane"), "Hello, Jane!");
+    Test.assertEquals(greet("Simon"), "Hello, Simon!");
+  });
+  Test.it("should greet Johnny a little bit more special",function(){
+    Test.assertEquals(greet("Johnny"), "Hello, my love!");
+  });
+});
