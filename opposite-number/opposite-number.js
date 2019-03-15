5fa0422e22a69d32da8ee7e6452b4cf9215c1833
PriscillaPalacin <priscipal7993@icloud.com>
2019-03-14 21:50:02 -0400

    opposite number code war

---
+++ opposite-number/opposite-number.js
@@ -0,0 +1,9 @@
+// Very simple, given a number, find its opposite.
+
+function opposite(number) {
+  //your code here
+  console.log(number)
+  return number * - 1 ;
+}
+
+//Test.assertEquals(opposite(1), -1,)
