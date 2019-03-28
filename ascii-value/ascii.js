f902e053214b72b551e7b6c6dea8afaf0b91552c
PriscillaPalacin <priscipal7993@icloud.com>
2019-03-27 23:10:15 -0400

    codewar

---
+++ ascii-value/ascii.js
@@ -0,0 +1,6 @@
+/*Write a function getChar/GetChar/get_char which takes a number and returns the corresponding ASCII char for that value.*/
+
+function getChar(c){
+  // ...
+  return String.fromCharCode(c)
+}
