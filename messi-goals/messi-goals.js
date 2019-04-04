e2d74639a0cc326b67b59786853118b8416b16c7
PriscillaPalacin <priscipal7993@icloud.com>
2019-04-03 20:22:28 -0400

    daily codewar

---
+++ messi-goals/messi-goals.js
@@ -0,0 +1,5 @@
+// complete the goals function to return his total goals (the sum) for all three leagues.
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+}
